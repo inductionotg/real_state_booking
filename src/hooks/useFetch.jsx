8b5257364cc3e7dc41d8840ddeeccb00559998fe
inductionotg <ritesh2000.sinha@gmail.com>
2024-04-14 11:17:44 +0530

    Added cache support in useEffect

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,16 +1,32 @@
-import { useRef,useState,useEffect } from "react"
+import { useRef,useState,useEffect,useMemo } from "react"
 import axios from "axios"
 import api from "@/api"
+import { getItem, setItem } from "@/lib/utils/localStorage"
 function useFetch(url,options){
     const [listing,setListing] = useState()
     const [error,setError]  = useState(null)
     const [loading,setLoading] = useState(true)
 
     const abortController  = useRef(null)
+    const STALE_VALUE = 5 *1000*60
+    const storageKey = useMemo(()=>{
+        if(!options?.params){
+            return url
+        }
+        return url + '?' +JSON.stringify(options.params)
+    })
 
     useEffect(()=>{
-
         async function fetchDetailListing(){
+            const currentTime = new Date().getTime()
+            const cachedData = getItem(storageKey)
+
+            if(cachedData && currentTime - cachedData.lastFetched < STALE_VALUE){
+                setListing(cachedData.listing)
+                setLoading(false)
+                return 
+            }
+
             setLoading(true)
             setError(null)
 
@@ -38,7 +54,17 @@ function useFetch(url,options){
         return ()=>{
             abortController.current?.abort()
         }
-    },[options,url])
+    },[options,url,storageKey])
+
+    useEffect(()=>{
+        if(!listing){
+            return 
+        }
+        setItem(storageKey,{
+            lastFetched:new Date().getTime(),
+            listing
+        })
+    },[listing,storageKey])
     return {listing,loading,error}
 }
 export default useFetch
